0bb6d092413566904ade988e668498b107e5362f
Olha <symchychnya@gmail.com>
2020-09-08 15:26:24 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/ninja.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
